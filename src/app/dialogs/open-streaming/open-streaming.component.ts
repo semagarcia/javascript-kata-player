84ae5761428168cd5e404706e2d8ecfbc71097f8
Sema García <jose-manuel.garcia@gft.com>
2017-03-10 20:41:00 +0100

    Added create-challenge & open-streaming dialog components

---
+++ src/app/dialogs/open-streaming/open-streaming.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { MdDialogRef } from '@angular/material';
+
+@Component({
+    templateUrl: './open-streaming.component.html'
+})
+export class OpenStreamingDialog implements OnInit {
+
+    private challengeId: string;
+    private isEmptyChallengeId: boolean;
+
+    constructor(public dialogRef:MdDialogRef<OpenStreamingDialog>) {}
+
+    ngOnInit() {
+        this.isEmptyChallengeId = false;
+    }
+
+    joinToChallenge() {
+        if(this.challengeId) {
+            this.dialogRef.close(this.challengeId);
+        } else {
+            this.isEmptyChallengeId = true;
+        }
+    }
+
+    cancel() {
+        this.dialogRef.close();
+    }
+
+}
