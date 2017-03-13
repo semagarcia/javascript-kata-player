ae42125bb6fb97ab7e5bfc9e2935323c5fe33659
Sema García <jose-manuel.garcia@gft.com>
2017-03-13 02:53:23 +0100

    Update create-challenge dialogs

--- src/app/dialogs/create-challenge/create-challenge.component.ts
+++ src/app/dialogs/create-challenge/create-challenge.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MdDialogRef } from '@angular/material';
+import { ChallengeService, SocketService } from './../../core';
 
 @Component({
     templateUrl: './create-challenge.component.html'
@@ -7,29 +9,73 @@ import { MdDialogRef } from '@angular/material';
 export class CreateChallengeDialog implements OnInit {
 
     private joinForm: boolean;
+    private challengeId: string;
+    private existsChallengeId: boolean;
     private isCreatingChallenge: boolean;
+    private joiningMessageError: string;
 
-    constructor(public dialogRef: MdDialogRef<CreateChallengeDialog>) {}
+    constructor(private dialogRef: MdDialogRef<CreateChallengeDialog>, 
+                private router: Router,
+                private challengeSrv: ChallengeService,
+                private socketSrv: SocketService) {}
 
     ngOnInit() {
         this.joinForm = false;
+        this.existsChallengeId = true;
         this.isCreatingChallenge = false;
+        this.joiningMessageError = '';
     }
 
-    createChanllenge() {
+    createChallenge() {
         this.showProgressBar(); 
         this.joinForm = false;
-        //this.dialogRef.close(true);   
+        let playerSocketId = this.socketSrv.getSocketId();
+        if(playerSocketId) {
+            this.challengeSrv.createChallengeId(playerSocketId).subscribe((challengeId) => { 
+                this.challengeId = challengeId;
+                this.isCreatingChallenge = false;
+            });
+        } else {
+            this.isCreatingChallenge = false;
+            this.joiningMessageError = 'ERR-SOCK-1000';
+        }
+    }
+
+    goToChallenge() {
+        this.router.navigate(['/challenge', this.challengeId]);
+        this.dialogRef.close();
     }
 
     showJoinToChallengeForm() {
         this.joinForm = !this.joinForm;
+        this.joiningMessageError = '';
         this.hideProgressBar();
     }
 
     joinToChallenge() {
-        //this.dialogRef.close(false);
-        this.showProgressBar();
+        this.joiningMessageError = '';  
+        if(this.challengeId) {
+            this.challengeSrv.checkIfChallengeIdExists(this.challengeId).subscribe(
+                (exists) => {
+                    if(exists) {
+                        this.router.navigate(['/challenge', this.challengeId]);
+                        this.dialogRef.close();
+                    } else {
+                        this.existsChallengeId = exists;
+                        this.joiningMessageError = 'Sorry, the given identifier not exists.';
+                    }
+                },
+                (error) => {
+                    this.joiningMessageError = error;
+                }
+            );
+        } else {
+            this.joiningMessageError = 'Please, you have to write a challenge identifier.';
+        }
+    }
+
+    private closeDialog() {
+        this.dialogRef.close();
     }
 
     private showProgressBar() {
