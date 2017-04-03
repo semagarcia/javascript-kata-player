cdfee0b0f9428e187031486751011a874e775c01
Sema García <jose-manuel.garcia@gft.com>
2017-04-04 01:07:09 +0200

    Added index.ts to better organisation

---
+++ src/app/dialogs/index.ts
@@ -0,0 +1,4 @@
+export * from './create-challenge/create-challenge.component';
+export * from './leave-challenge/leave-challenge.component';
+export * from './open-streaming/open-streaming.component';
+export * from './select-training-path/select-training-path.component';
