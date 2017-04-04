05c4c450c04552b30a14f17a56668be41c7e4368
Sema García <jose-manuel.garcia@gft.com>
2017-04-04 18:12:35 +0200

    Refactorized the training view to reuse the kata-player component

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,29 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, animate, style, state, transition, trigger } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-
-import { KataMetadata, TrainingService } from './../core';
-
 import { Observable } from 'rxjs';
 
+import { KataExercise, TrainingService } from './../core';
+
 @Component({
-    selector: 'app-training',
     templateUrl: './training.component.html',
     styleUrls: ['./training.component.scss']
 })
 export class TrainingComponent implements OnInit {
 
     private trainingPath: string;
-    private trainingPathMetadata: KataMetadata;
+    private currentExerciseIndex: number;
+    private currentExercise: KataExercise;
+    private pathExercises: Array<KataExercise>;
+    private selectedExercise: string;
+    private selectedValue: string;
+    private trainingPathLength: number;
 
     constructor(private router: Router, private route: ActivatedRoute, private trainingSrv: TrainingService) { }
 
     ngOnInit() {
+        this.currentExerciseIndex = 0;
         this.route.params.subscribe(params => {
             this.trainingPath = params['path'];
             this.trainingSrv.getPathExercises(this.trainingPath).subscribe(
-                (metadata: KataMetadata) => { this.trainingPathMetadata = metadata; }
+                (exercises: Array<KataExercise>) => { 
+                    // Copy all exercises
+                    this.pathExercises = exercises;  
+                    this.trainingPathLength = this.pathExercises.length;
+
+                    // Select the first of them as initial exercise
+                    this.currentExercise = exercises[this.currentExerciseIndex];
+                    this.selectedValue = exercises[this.currentExerciseIndex].id;
+                }
             );
         });
+    }
+
+    onSelectedChange(e) {
+        let selected: KataExercise = this.pathExercises.filter(
+            (exercise: KataExercise) => { return (exercise.id === e.value); }
+        )[0];
+        this.selectedValue = selected.name;
+    }
+
+    onSuccessKata() {
+
+    }
+
+    onFailedKataAttemp() {
+
+    }
+
+    onNextExercise() {
+        this.nextExerciseIndex();
+        this.currentExercise = this.pathExercises[this.currentExerciseIndex];
+        this.selectedValue = this.pathExercises[this.currentExerciseIndex].id;
+    }
+
+    nextExerciseIndex() {
+        this.currentExerciseIndex = (this.currentExerciseIndex !== this.pathExercises.length-1) ? ++this.currentExerciseIndex : 0; 
     }
 
 }
