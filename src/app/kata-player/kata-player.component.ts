2b32088ba1e0467785b1e3aaa52eabe5872f9343
Sema García <jose-manuel.garcia@gft.com>
2017-04-19 22:34:52 +0200

    Added animation and logic to show editor pane with an effect and added a tooltip

--- src/app/kata-player/kata-player.component.ts
+++ src/app/kata-player/kata-player.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter, animate, state, style, transition, trigger } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable, Subscription } from 'rxjs';
 import { MdDialog } from '@angular/material';
@@ -9,13 +9,27 @@ import { TimeElapsedPipe, TestExecutorService } from './../core';
 import 'codemirror/mode/javascript/javascript';
 
 @Component({
-  selector: 'kata-player',
-  templateUrl: './kata-player.component.html',
-  styleUrls: ['./kata-player.component.scss']
+    selector: 'kata-player',
+    templateUrl: './kata-player.component.html',
+    styleUrls: ['./kata-player.component.scss'],
+    animations: [
+        trigger('startKata', [
+            transition(':enter', [
+                style({ opacity: 0 })
+            ]),
+            state('reading', style({
+                opacity: 0
+            })),
+            state('writing', style({
+                opacity: 1
+            })),
+            transition('* => *', animate('400ms ease-in-out'))
+        ])
+    ]
 })
 export class KataPlayerComponent implements OnInit, OnChanges {
 
-    private showEditorPane: boolean;
+    private kataState: string = 'reading';
     private leftPaneWidth: number;
     private resizingModeEnabled: boolean;
     private bodyFunction;
@@ -34,13 +48,14 @@ export class KataPlayerComponent implements OnInit, OnChanges {
     @Output() fail = new EventEmitter();
     @Output() next = new EventEmitter();
 
-    constructor(private httpSrv: Http, private testExecutorSrv: TestExecutorService, public dialog: MdDialog) {}
+    constructor(private httpSrv: Http, private testExecutorSrv: TestExecutorService, public dialog: MdDialog) {
+        this.kataState = 'reading';
+    }
 
     ngOnInit() {
-        this.showEditorPane = false;
         this.leftPaneWidth = 50;
         this.resizingModeEnabled = false;
-        //this.bodyFunction = this.code;
+
         this.config = {
             cursorBlinkRate: 200,
             lineNumbers: true,
@@ -53,8 +68,6 @@ export class KataPlayerComponent implements OnInit, OnChanges {
         this.counterDownObs = Observable.timer(0, 1000).subscribe((tick) => {
             this.timeSpent++;
         });
- 
-        this.showEditorPane = true;
     }
 
     ngOnChanges() {
@@ -63,23 +76,25 @@ export class KataPlayerComponent implements OnInit, OnChanges {
     }
 
     startExercise() {
-        
+        this.kataState = 'writing';
     }
 
     testKata() {
-        this.testExecutorSrv.checkExerciseCode(this.bodyFunction).subscribe(
-            (result: any) => {
-                if(result.executionResult && result.output) {
-                    this.testResultOutput = this.testExecutorSrv.formatOutput(result.output.split('\n'));
-                } else if(result.executionResult && !result.output) {
-                    this.testResultOutput = 'Not found nothing to test...';
-                } else if(!result.executionResult) {
-                    this.testResultOutput = this.testExecutorSrv.formatOutput(result.output.split('\n'));
-                    this.counterDownObs.unsubscribe();
-                    this.success.emit(this.timeSpent);
+        if(this.kataState === 'writing') {
+            this.testExecutorSrv.checkExerciseCode(this.bodyFunction).subscribe(
+                (result: any) => {
+                    if(result.executionResult && result.output) {
+                        this.testResultOutput = this.testExecutorSrv.formatOutput(result.output.split('\n'));
+                    } else if(result.executionResult && !result.output) {
+                        this.testResultOutput = 'Not found nothing to test...';
+                    } else if(!result.executionResult) {
+                        this.testResultOutput = this.testExecutorSrv.formatOutput(result.output.split('\n'));
+                        this.counterDownObs.unsubscribe();
+                        this.success.emit(this.timeSpent);
+                    }
                 }
-            }
-        );
+            );
+        }
     }
 
     stop() {
@@ -92,14 +107,15 @@ export class KataPlayerComponent implements OnInit, OnChanges {
     }
 
     mousedown(e) {
-        //this.resizingModeEnabled = true;
+
     }
 
     mousemove(e) {
+
     }
 
     mouseup(e) {
-        //this.resizingModeEnabled = false;
+
     }
 
 }
