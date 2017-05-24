bf6aa68ab9488a990f3b29ca02cd64ed611116b6
Sema García <jose-manuel.garcia@gft.com>
2017-05-24 19:18:28 +0200

    Updated login view with event selector (optional)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,21 +11,29 @@ export class LoginComponent implements OnInit {
 
     username: string;
     password: string;
+    events: Array<string>;
+    eventSelected: string;
+    //languages: Array<string>;
 
     constructor(private router: Router, private loginSrv: LoginService) { }
 
     ngOnInit() {
         this.username = '';
         this.password = '';
+        this.eventSelected = '';
+        this.loginSrv.getCurrentEvents().subscribe(
+            (events) => this.events = events,
+            (err) => this.events = []
+        );
     }
 
     standardLogin() {
-        //this.loginSrv.standardLogin(this.username, this.email).subscribe(
-        //    (response) => {
+        this.loginSrv.login(this.username, this.password, this.eventSelected).subscribe(
+            (response) => {
                 this.router.navigate(['home']);
-        //    },
-        //    (err) => { console.log('Error: ', err); }
-        //);
+            },
+            (err) => { console.log('Error: ', err); }
+        );
     }
 
 }
