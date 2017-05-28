008b0f00245192808506bf3df63d9945336009df
Sema García <jose-manuel.garcia@gft.com>
2017-05-28 05:18:11 +0200

    Hide training option when an event is selected in standard login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MdDialog } from '@angular/material';
+
 import { CreateChallengeDialog, SelectTrainingPathDialog } from './../dialogs';
+import { UserService } from './../core/services/user.service';
 
 @Component({
     templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+    event: string;
 
-    constructor(private router: Router, public dialog: MdDialog) { }
+    constructor(private router: Router, public dialog: MdDialog, public userSrv: UserService) { }
+
+    ngOnInit() {
+        this.event = this.userSrv.getUserContext().event;
+    }
 
     startIndividual() {
         this.router.navigate(['/individual']);
