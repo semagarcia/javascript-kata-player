1f6a69b080200fad5e57933b1133b517e8499c58
Sema García <jose-manuel.garcia@gft.com>
2017-05-30 02:35:57 +0200

    Updated about component

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    // {name:string, contribution:string, pic:string, bio:string}
+    contributors: Array<any>;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.contributors = [
+            { 
+                name: 'Sema García', 
+                contribution: 'Creator of KataPlayer & Contributor', 
+                pic: 'https://avatars2.githubusercontent.com/u/10792397?v=3&s=300', 
+                bio: 'Lorem ipsum...',
+                twitter: '',
+                linkedin: '' 
+            },
+            { 
+                name: 'Adrián Ferreres Esteller', 
+                contribution: 'Full-stack Contributor', 
+                pic: 'https://avatars0.githubusercontent.com/u/3256924?v=3&s=300', 
+                bio: 'Lorem ipsum...',
+                twitter: '',
+                linkedin: '' 
+            },
+            { 
+                name: 'Gonzalo Ruiz de Villa', 
+                contribution: 'Kata Engine Contributor', 
+                pic: 'https://avatars0.githubusercontent.com/u/9501?v=3&s=300', 
+                bio: 'Lorem ipsum...',
+                twitter: '',
+                linkedin: '' 
+            }
+        ];
+    }
 
 }
