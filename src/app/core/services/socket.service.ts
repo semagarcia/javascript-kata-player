e765814fcedffeb9d74827947c0dae4e52a583c6
Sema García <jose-manuel.garcia@gft.com>
2017-06-19 04:25:08 +0200

    Updated and parameterized socket service

--- src/app/core/services/socket.service.ts
+++ src/app/core/services/socket.service.ts
@@ -10,54 +10,24 @@ export class SocketService {
     socket: SocketIOClient.Socket;
     
     constructor() {
-        /*this.socket = io.connect(this.url);
-        this.socket.on("connect", () => console.log('connect: '+this.socket.id));
-        this.socket.on("disconnect", () => console.log('d'));
-        this.socket.on('error', (error: string) => {
-            console.log(`ERROR: "${error}" (${this.url})`);
-        });*/
-
         if(environment.production) {
-            //this.socket = io();
             this.socket = io.connect();
         } else {
             this.url = `${window.location.protocol}//${window.location.hostname}:3000`;
-            console.log('Connecting socket to ', this.url);
             this.socket = io.connect(this.url);
             this.sendMessage('message', {message: 'hello'});
         }
-        
-        let listener = Observable.fromEvent(this.socket, 'msg')
-            .subscribe((payload) => { console.log('MSG: ', payload); });
-        /*let listener2 = Observable.fromEvent(this.socket, 'code')
-            .subscribe((payload) => { console.log('Payload2: ', payload); });*/
-        
-        /*let obs = new Observable(observer => {
-            this.socket = io.connect(this.url);
-            this.socket.on('msg', (payload) => { observer.next(payload) });
-            this.socket.on('msg2', (payload) => { observer.next(payload) });
-            return () => { this.socket.disconnect(); };
-        });
-        obs.subscribe((data) => { console.log('>> ', data); });*/
     }
 
     getSocketId(): string {
         return (this.socket) ? this.socket.id : '';
     }
 
-    connectToStreaming() {
-        return Observable.fromEvent(this.socket, 'challenge');
+    connectToStreaming(socketEventName) {
+        return Observable.fromEvent(this.socket, socketEventName);
     }
 
-    /*get(): Observable<any> {
-        return Observable.create((observer: any) => {
-            this.socket.on('msg', (item: any) => { console.log('Msg received: ', item); });
-            return () => this.socket.close();
-        });
-    }*/
-
     sendMessage(type: string, payload: object) {
-        //this.socket.send(type, payload);
         this.socket.emit(type, payload);
     }
 
