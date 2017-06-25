e4e030244b043182ca9b9dabaff47036153a3099
Sema García <jose-manuel.garcia@gft.com>
2017-06-25 23:32:51 +0200

    Add experimental/temporal control to avoid create new connections

--- src/app/core/services/socket.service.ts
+++ src/app/core/services/socket.service.ts
@@ -10,12 +10,16 @@ export class SocketService {
     socket: SocketIOClient.Socket;
     
     constructor() {
+        if(this.socket) {
+            console.log('SS >>>> ', this.socket.connected);
+            this.socket.removeAllListeners();
+            this.socket.disconnect();
+        }
         if(environment.production) {
             this.socket = io.connect({ transports: ['websocket'], upgrade: false });
         } else {
             this.url = `${window.location.protocol}//${window.location.hostname}:3000`;
             this.socket = io.connect(this.url, { transports: ['websocket'], upgrade: false });
-            this.sendMessage('message', {message: 'hello'});
         }
     }
 
