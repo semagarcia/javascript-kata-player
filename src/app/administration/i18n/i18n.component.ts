b9bf028fad4512bdc3aeff220b11c1c64fa3085b
Sema García <jose-manuel.garcia@gft.com>
2017-07-24 21:37:59 +0200

    Add the basic structure for fill the content of the section

--- src/app/administration/i18n/i18n.component.ts
+++ src/app/administration/i18n/i18n.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminAgGridMaterialCheckbox } from './../admin-ag-grid-checkbox';
+import { GridOptions } from "ag-grid/main";
 
 @Component({
     selector: 'admin-i18n',
@@ -7,9 +9,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class I18NComponent implements OnInit {
 
-    constructor() { }
+    gridOptions: GridOptions = {};
+
+    constructor() { 
+        // Default options
+        this.gridOptions.defaultColDef = { 
+            width: 50,
+            editable: true,
+            filter: 'text'
+        };
+        
+        // Configure grid
+        this.gridOptions.rowHeight = 48;
+        this.gridOptions.editType = 'fullRow';
+        this.gridOptions.animateRows = true;
+        this.gridOptions.enableFilter = true;
+        this.gridOptions.enableSorting = true;
+        this.gridOptions.enableColResize = true;
+        this.gridOptions.singleClickEdit = false;
+        this.gridOptions.stopEditingWhenGridLosesFocus = true;
+        this.gridOptions.sortingOrder = ['desc', 'asc', null];
+        this.gridOptions.rowSelection = 'multiple';
+        this.gridOptions.icons = {
+            checkboxChecked: AdminAgGridMaterialCheckbox.CB_ICON
+        };
+
+        this.gridOptions.onRowEditingStarted = (event) => {
+            //this.auxEditRow = Object.assign({}, event.node.data);
+        };
+
+        this.gridOptions.onRowEditingStopped = (event) => {
+            /*if(!this.isTheSameEventRow(this.auxEditRow, event.node.data))
+                this.editEvent(event.node.data);*/
+        };
+
+        this.gridOptions.columnDefs = [
+            {
+                headerName: '', 
+                field: 'selectRow',
+                headerCheckboxSelection: true,
+                headerCheckboxSelectionFilteredOnly: true,
+                checkboxSelection: true,
+                suppressFilter: true,
+                width: 25
+            },
+            {
+                headerName: 'Language',
+                field: 'language',
+                width: 120,
+                filter: 'text'
+            },
+            {
+                headerName: 'Enabled?',
+                field: 'enabled',
+                width: 70,
+                cellRenderer: (params) => { return (params.value) ? 'Yes' : 'No' },
+                cellEditor: 'select',
+                cellEditorParams: {
+                    values: ['Yes', 'No']
+                },
+                cellClass: 'center-column-content'
+            }
+        ];
+    }
 
     ngOnInit() {
+    }
+
+    addNewLanguage() {
+
+    }
+
+    editLanguage() {
+
+    }
+
+    deleteLanguage() {
+
     }
 
 }
